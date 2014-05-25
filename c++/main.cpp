c25242086a686585870a70bb1b2317bdc32c1aeb
filtoid <philjeffes@hotmail.com>
2014-05-25 22:21:52 +0100

    Initial commit of the C++ code which will read in the getdata.php output and dump the json to the output

---
+++ c++/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <curl/curl.h>
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.philjeffes.co.uk/twitter/getdata.php?q=searchterm");
+
+    /* Perform the request, res will get the return code */ 
+    res = curl_easy_perform(curl);
+    /* Check for errors */ 
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
