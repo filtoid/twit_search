60ed82f9ec037b3ae4d23913fa8d23c8e7d3283d
filtoid <philjeffes@hotmail.com>
2014-05-26 12:42:14 +0100

    Included the processing of the JSON returned from the web request. This is then output to the screen.

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include <curl/curl.h>
 #include <json/json.h>
+#include <list>
 
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
-}
+using namespace std;
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
+list<string> getListStatusStrings(string input);
 
 int main(void)
 {
   CURL *curl;
   CURLcode res;
-  std::string readBuffer;
+  string readBuffer;
+
+	cout << "Starting the request" << endl;
 
   curl = curl_easy_init();
   if(curl) {
@@ -31,11 +34,49 @@ int main(void)
     	curl_easy_cleanup(curl);
   }
 
-
+	cout << "Finished the request" << endl;
+	cout << "Starting the JSON Parsing" << endl;
   
-  Json::Value root;   // will contains the root value after parsing.
-  //Json::Reader reader;
+	list<string> statusList = getListStatusStrings(readBuffer);
 
-  return 0;
+	for (std::list<string>::iterator it=statusList.begin(); it != statusList.end(); ++it){
+    		std::cout << ' ' << *it << endl<< endl;
+	}
+
+ 	return 0;
+}
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+/*
+*This code has been taken from (and altered from) the sourceforge documentation 
+*which can be found here: http://jsoncpp.sourceforge.net/
+*/
+list<string> getListStatusStrings(string input)
+{
+	list<string> ret;
+
+	Json::Value root;   // will contains the root value after parsing.
+	Json::Reader reader;
+	bool parsingSuccessful = reader.parse( input, root );
+	if ( !parsingSuccessful )
+	{
+	    cout  << "Failed to parse input:\n"
+		       << "    " << reader.getFormattedErrorMessages();
+	    return ret;
+	}
+
+	const Json::Value statuses = root["statuses"];
+	for ( int index = 0; index < statuses.size(); ++index )
+	{  
+	   Json::Value status = statuses[index];
+	   ret.push_back( status["text"].asString() );
+	}
+
+	return ret;
 }
 
