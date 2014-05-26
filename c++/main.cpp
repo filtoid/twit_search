e37c8d4bb983c674bf5377c86cbccf301dd2d86e
filtoid <philjeffes@hotmail.com>
2014-05-26 14:35:23 +0100

    The application now needs args to run - a search term for arg 1 and an output file name for arg2. This then outputs the results to a file - ready for processing.

--- c++/main.cpp
+++ c++/main.cpp
@@ -9,40 +9,58 @@ using namespace std;
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
 list<string> getListStatusStrings(string input);
 
-int main(void)
+int main(int argc, char* argv[])
 {
-  CURL *curl;
-  CURLcode res;
-  string readBuffer;
+
+	if(argc!=3){
+		cout<< "The number of arguments provided is not correct." <<endl;
+		cout<< "Please supply a search term as arg1 and an output"<<endl;
+		cout<< "and an output file name."<<endl;
+		cout<< endl << "eg. ./twit_search \"search term\" output.txt"<<endl;
+		return 0;
+	}
+	string searchTerm = argv[1];
+	string fn = argv[2];
+
+	string urlfull = "http://www.philjeffes.co.uk/twitter/getdata.php?q=";
+	urlfull += searchTerm;
+
+	CURL *curl;
+	CURLcode res;
+	string readBuffer;
 
 	cout << "Starting the request" << endl;
-
-  curl = curl_easy_init();
-  if(curl) {
-    	curl_easy_setopt(curl, CURLOPT_URL, "http://www.philjeffes.co.uk/twitter/getdata.php?q=searchterm");
-	readBuffer.clear();
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-	/* Perform the request, res will get the return code */ 
-	res = curl_easy_perform(curl);
-	/* Check for errors */ 
-	if(res != CURLE_OK)
-      		fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              	curl_easy_strerror(res));
- 
-    	/* always cleanup */ 
-    	curl_easy_cleanup(curl);
-  }
+	
+	curl = curl_easy_init();
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, urlfull.c_str());
+		readBuffer.clear();
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+		/* Perform the request, res will get the return code */ 
+		res = curl_easy_perform(curl);
+		/* Check for errors */ 
+		if(res != CURLE_OK)
+	      		fprintf(stderr, "curl_easy_perform() failed: %s\n",
+		      	curl_easy_strerror(res));
+	 
+	    	/* always cleanup */ 
+	    	curl_easy_cleanup(curl);
+	}
 
 	cout << "Finished the request" << endl;
 	cout << "Starting the JSON Parsing" << endl;
   
 	list<string> statusList = getListStatusStrings(readBuffer);
+	cout << statusList.size() << " tweets found" <<endl;
 
-	for (std::list<string>::iterator it=statusList.begin(); it != statusList.end(); ++it){
-    		std::cout << ' ' << *it << endl<< endl;
+	ofstream outputFile;
+	outputFile.open(fn.c_str());
+	
+	for (list<string>::iterator it=statusList.begin(); it != statusList.end(); ++it){
+    		outputFile << *it << endl<< endl;
 	}
-
+	outputFile.close();
  	return 0;
 }
 
